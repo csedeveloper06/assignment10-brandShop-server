5b0ff27e3c086010866adfb7b55c52c7e9e27018
farida farida <faridayasmeencse@gmail.com>
2023-10-25 17:31:40 +0600

    added delete method for carts in the My Cart page

--- index.js
+++ index.js
@@ -135,6 +135,14 @@ async function run() {
       res.send(result);
   })
 
+
+    app.delete('/carts/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result);
+    })
+
     
 
     // Send a ping to confirm a successful connection
